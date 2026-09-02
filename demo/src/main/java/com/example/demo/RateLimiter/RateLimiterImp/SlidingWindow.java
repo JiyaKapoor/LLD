f66e099477ead7aa9fda604b93b2f6e1591c43fe
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-02 19:14:12 +0530

    Sliding window algo config defined

--- demo/src/main/java/com/example/demo/RateLimiter/RateLimiterImp/SlidingWindow.java
+++ demo/src/main/java/com/example/demo/RateLimiter/RateLimiterImp/SlidingWindow.java
@@ -3,7 +3,14 @@ package com.example.demo.RateLimiter.RateLimiterImp;
 import com.example.demo.RateLimiter.Entity.RateLimiter;
 import com.example.demo.RateLimiter.Entity.User;
 
+import java.util.Deque;
+import java.util.HashMap;
+
 public class SlidingWindow implements RateLimiter {
+    //in this algorithm we basically needc to maintain a sliding window which keeps track of the requests made uptill n milisec ago
+    private int maxRequests;
+    private Long windowSize;
+    private HashMap<Long, Deque<Long>> requestLog=new HashMap<>();
     public boolean allowRequest(User user){
 
     }
