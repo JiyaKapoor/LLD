c4e478c5a76605c2befef49f14f8dbe8f3f08797
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-02 15:27:20 +0530

    User entity added

---
+++ demo/src/main/java/com/example/demo/RateLimiter/Entity/User.java
@@ -0,0 +1,8 @@
+package com.example.demo.RateLimiter.Entity;
+
+public class User {
+    private String userId;
+    public String getUserId(){
+        return this.userId;
+    }
+}
--- demo/src/main/java/com/example/demo/RateLimiter/RateLimiterImp/TokenBucket.java
+++ demo/src/main/java/com/example/demo/RateLimiter/RateLimiterImp/TokenBucket.java
@@ -1,4 +1,9 @@
 package com.example.demo.RateLimiter.RateLimiterImp;
 
+import java.util.HashMap;
+
 public class TokenBucket {
+    //basically every user will be allotted a fixed number of tokens
+    HashMap<String,Integer> avalTokens=new HashMap<>();
+    HashMap<String,Long> refillTime=new HashMap<>();
 }
