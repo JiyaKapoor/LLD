b8098b6544ee57da9d4bee90c5f0339879c68010
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-03 12:36:03 +0530

    JPA dependency added in POM

--- demo/src/main/java/com/example/demo/RateLimiter/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/RateLimiter/Controller/UserController.java
@@ -1,8 +1,17 @@
 package com.example.demo.RateLimiter.Controller;
 
+import com.example.demo.RateLimiter.Entity.UserTier;
+import com.example.demo.RateLimiter.Service.RateLimiterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    private RateLimiterService rateLimiterService;
+    @PostMapping
+    public void registerUser(UserTier userTier){
 
+    }
 }
