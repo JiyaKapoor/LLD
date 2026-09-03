ae5ee8e5ede850f83aca4003285d0f726e2a53a9
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-03 12:53:01 +0530

    Controller fixed to test the service

--- demo/src/main/java/com/example/demo/RateLimiter/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/RateLimiter/Controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.demo.RateLimiter.Entity.User;
 import com.example.demo.RateLimiter.Entity.UserTier;
 import com.example.demo.RateLimiter.Service.RateLimiterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private RateLimiterService rateLimiterService;
-    @PostMapping
-    public void userRequest(Long userId,UserTier userTier){
-        rateLimiterService.allowRequest(userId,userTier);
+    @GetMapping
+    public ResponseEntity<String> userRequest(Long userId, UserTier userTier){
+        boolean res=rateLimiterService.allowRequest(userId,userTier);
+        if(res){
+            return ResponseEntity.ok("Request Reached Server");
+        }
+        return ResponseEntity.ok("Reqest Rate Limited, Try sometime later!");
     }
 }
