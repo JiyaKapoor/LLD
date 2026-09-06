ccb5307f266708b53e25d2e5447cb6241f6faa7c
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-06 11:54:39 +0530

    Food delivery system initialised

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
