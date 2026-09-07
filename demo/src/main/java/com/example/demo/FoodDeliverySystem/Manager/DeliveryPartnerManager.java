403d0db9c1219c80f1d64a95f9174ef3719e10da
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-07 13:15:52 +0530

    Delivey partner manager business logic done

--- demo/src/main/java/com/example/demo/FoodDeliverySystem/Manager/DeliveryPartnerManager.java
+++ demo/src/main/java/com/example/demo/FoodDeliverySystem/Manager/DeliveryPartnerManager.java
@@ -3,8 +3,14 @@ package com.example.demo.FoodDeliverySystem.Manager;
 import com.example.demo.FoodDeliverySystem.Entity.DeliveryPartner;
 import com.example.demo.FoodDeliverySystem.Entity.Restaurant;
 
-public class DeliveryPartnerManager {
-    public static DeliveryPartner assignPartner(Restaurant restaurant){
+import java.util.List;
 
+public class DeliveryPartnerManager {
+    static List<DeliveryPartner> deliveryPartners;
+    public static void addDeliveryPartner(DeliveryPartner deliveryPartner){
+        deliveryPartners.add(deliveryPartner);
+    }
+    public static DeliveryPartner assignPartner(Restaurant restaurant){
+        //we assign the nearest delivery partner
     }
 }
