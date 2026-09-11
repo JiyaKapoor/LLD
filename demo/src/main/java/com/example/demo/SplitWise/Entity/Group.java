2ab383e87d5006498e7dc52a300f81a92bba7888
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-11 14:11:21 +0530

    User removal and addition methods and logic added

--- demo/src/main/java/com/example/demo/SplitWise/Entity/Group.java
+++ demo/src/main/java/com/example/demo/SplitWise/Entity/Group.java
@@ -8,5 +8,16 @@ public class Group{
     List<User> users;
     String groupName;
     HashMap<String,HashMap<String,Double>> balance;
-
+    public void addUser(User user){
+        this.users.add(user);
+    }
+    public boolean removeUser(User user){
+        //this user can only leave if they have settled all the expenses
+        HashMap<String,Double> moneyOwed=balance.get(user.userId);
+        if(moneyOwed.isEmpty()){
+            users.remove(user);
+            return true;
+        }
+        return false;
+    }
 }
