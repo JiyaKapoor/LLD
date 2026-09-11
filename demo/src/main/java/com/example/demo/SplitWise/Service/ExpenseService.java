9d731ef27d628f8daf408d588e9aa82db842f6db
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-11 19:01:02 +0530

    addExpense method calling factory method to create the req splitStrategy and calling calcSplit method on the req splitStrat

--- demo/src/main/java/com/example/demo/SplitWise/Service/ExpenseService.java
+++ demo/src/main/java/com/example/demo/SplitWise/Service/ExpenseService.java
@@ -1,23 +1,23 @@
 package com.example.demo.SplitWise.Service;
 
-import com.example.demo.FoodDeliverySystem.Entity.User;
+import com.example.demo.SplitWise.Entity.User;
 import com.example.demo.SplitWise.Entity.Expense;
 import com.example.demo.SplitWise.Entity.Group;
 import com.example.demo.SplitWise.Entity.SplitStrategy;
 import com.example.demo.SplitWise.Entity.SplitType;
 import com.example.demo.SplitWise.Factory.SplitStrategyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
+import java.util.Map;
 
 public class ExpenseService {
     @Autowired
     SplitStrategyFactory splitStrategyFactory;
     List<Expense> expenses;
-    public void addExpense(Group group, User payer, double amount, SplitType splitType){
+    public void addExpense(List<User> participants, User payer, double amount, SplitType splitType, Map<User,Double> share){
         //STEP 1: create the appropriate SplitStrategy object using the function parameters
         SplitStrategy splitStrategy=splitStrategyFactory.createSplit(splitType);
         // Now if we have an equal split in the function param-> equalsplitStrategy instance created
-
+        Map<User,Double> moneyOwed=splitStrategy.calculateSplit(amount,participants,share);
     }
 }
