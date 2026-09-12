0a091f5053f0b88fb1323aa56ca11ca281068f7e
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-12 09:36:06 +0530

    SplitStrategy factory completed

--- demo/src/main/java/com/example/demo/SplitWise/Factory/SplitStrategyFactory.java
+++ demo/src/main/java/com/example/demo/SplitWise/Factory/SplitStrategyFactory.java
@@ -1,12 +1,18 @@
 package com.example.demo.SplitWise.Factory;
 
+import com.example.demo.SplitWise.Entity.EqualSplitStrategy;
+import com.example.demo.SplitWise.Entity.PercentageSplitStrategy;
 import com.example.demo.SplitWise.Entity.SplitStrategy;
 import com.example.demo.SplitWise.Entity.SplitType;
 
 public class SplitStrategyFactory {
     public SplitStrategy createSplit(SplitType splitType){
         if(splitType==SplitType.EQUAL){
-
+            return new EqualSplitStrategy();
         }
+        else if(splitType==SplitType.PERCENTAGE){
+            return new PercentageSplitStrategy();
+        }
+        return null;//no valid splitStrategy entered
     }
 }
