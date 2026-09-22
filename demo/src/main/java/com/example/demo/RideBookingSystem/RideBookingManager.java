985b14ddd7c7e669132b2bb8c4291bc3b2ad2f25
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-22 18:52:26 +0530

    Facade pattern implemented by making the RideBooking manager delegating tasks to all the subsystems

--- demo/src/main/java/com/example/demo/RideBookingSystem/RideBookingManager.java
+++ demo/src/main/java/com/example/demo/RideBookingSystem/RideBookingManager.java
@@ -1,12 +1,37 @@
 package com.example.demo.RideBookingSystem;
 
-import com.example.demo.RideBookingSystem.Entity.Driver;
-import com.example.demo.RideBookingSystem.Entity.RideType;
+import com.example.demo.RideBookingSystem.Entity.*;
+import com.example.demo.RideBookingSystem.Service.DriverService;
+import com.example.demo.RideBookingSystem.Service.RideService;
+import com.example.demo.RideBookingSystem.Strategy.DriverMatchingStrategy;
+import com.example.demo.RideBookingSystem.Strategy.PricingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RideBookingManager {
-
+    @Autowired
+    DriverService driverService;
+    @Autowired
+    RideService rideService;
+    public void registerDriver(String driverId, String driverName, Vehicle vehicle){
+        driverService.registerDriver(driverId,driverName,vehicle);
+    }
+    public Trip requestRide(User user, Location destLoc, RideType rideType){
+        return rideService.requestRide(user,destLoc,rideType);
+    }
+    public void startTrip(Trip trip){
+        rideService.startTrip(trip);
+    }
+    public void endTrip(Trip trip){
+        rideService.endTrip(trip);
+    }
+    public void setPricingStrategy(PricingStrategy pricingStrategy){
+        rideService.setPricingStrategy(pricingStrategy);
+    }
+    public void setDriverMetchingStrategy(DriverMatchingStrategy driverMatchingStrategy){
+        rideService.setDriverMatchingStrategy(driverMatchingStrategy);
+    }
 }
