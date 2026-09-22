787abac507fceec7592ab02944eb5701e14139e5
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-22 14:02:43 +0530

    Logger class added to test the code

---
+++ demo/src/main/java/com/example/demo/LoggingFramework/DebugLogHandler.java
@@ -0,0 +1,14 @@
+package com.example.demo.LoggingFramework;
+
+public class DebugLogHandler extends LogHandler{
+
+    @Override
+    boolean canHandle(LogType logType) {
+        return logType==LogType.DEBUG;
+    }
+
+    @Override
+    void handle(String message) {
+        System.out.println("[DEBUG]"+message);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/LoggingFramework/Logger.java
@@ -0,0 +1,7 @@
+package com.example.demo.LoggingFramework;
+
+public class Logger {
+    public static void main(String[] args){
+
+    }
+}
