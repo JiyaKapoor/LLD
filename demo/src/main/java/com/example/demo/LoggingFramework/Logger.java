b6117ac8d88bdb1ec69869306d8d83919626d9c5
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-22 14:05:09 +0530

    LogHandler chain created and tested

--- demo/src/main/java/com/example/demo/LoggingFramework/Logger.java
+++ demo/src/main/java/com/example/demo/LoggingFramework/Logger.java
@@ -2,6 +2,10 @@ package com.example.demo.LoggingFramework;
 
 public class Logger {
     public static void main(String[] args){
-
+        LogHandler infoLogHandler=new InfoLogHandler();
+        LogHandler debugLogHandler=new DebugLogHandler();
+        infoLogHandler.setNext(debugLogHandler);
+        infoLogHandler.log(LogType.INFO,"User abc signed in");
+        infoLogHandler.log(LogType.DEBUG,"No record found in DB");
     }
 }
