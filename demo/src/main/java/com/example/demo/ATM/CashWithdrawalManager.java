1488bba2be5e245251680bae45cb16f793f8bd63
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-23 16:58:42 +0530

    ThousandHandler implemented

--- demo/src/main/java/com/example/demo/ATM/CashWithdrawalManager.java
+++ demo/src/main/java/com/example/demo/ATM/CashWithdrawalManager.java
@@ -1,6 +1,10 @@
 package com.example.demo.ATM;
 
 public abstract class CashWithdrawalManager {
+    CashWithdrawalManager next;
+    public void setNext(CashWithdrawalManager next){
+        this.next=next;
+    }
     public abstract void dispense(double amount);
 
 
--- demo/src/main/java/com/example/demo/ATM/ThousandHandler.java
+++ demo/src/main/java/com/example/demo/ATM/ThousandHandler.java
@@ -1,8 +1,18 @@
 package com.example.demo.ATM;
 
 public class ThousandHandler extends CashWithdrawalManager{
+    int numNotes;
+    public ThousandHandler(int numNotes){
+        this.numNotes=numNotes;
+    }
     @Override
     public void dispense(double amount){
-
+        int notesReq=(int)(amount/1000.0);
+        int notesTobeDispensed=Math.min(numNotes,notesReq);
+        System.out.println("Dispensing "+notesTobeDispensed+" thousand notes");
+        double remainingAmt=amount-notesTobeDispensed*1000;
+        if(remainingAmt>0){
+            this.next.dispense(remainingAmt);//forwarding the req down the chain
+        }
     }
 }
