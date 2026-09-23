b4ba2da39116fef38607d59e3b40e510b1507a58
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-09-23 17:04:40 +0530

    All the core methods imp in the controller ATM class that implements them via the currState object

--- demo/src/main/java/com/example/demo/ATM/ATM.java
+++ demo/src/main/java/com/example/demo/ATM/ATM.java
@@ -5,9 +5,21 @@ public class ATM {
     CashWithdrawalManager cashWithdrawalManager;
     public ATM(){
         this.currState=new IdleState();
-
+        CashWithdrawalManager thousandHandler=new ThousandHandler(10);
+        CashWithdrawalManager fiveHundredHandler=new FiveHundredHandler(50);
+        thousandHandler.setNext(fiveHundredHandler);//chain formed
     }
     public void setState(ATMState atmState){
         this.currState=atmState;
+    }
+    //defining all the calls that can be made on the ATM
+    public void insertCard(Card card){
+        currState.insertCard(this);
+    }
+    public void enterPIN(Card card,int PIN){
+        currState.enterPIN(this,card,PIN);
+    }
+    public void withdrawCash(Card card,double amt){
+        currState.withdrawCash(this,card,amt);
     }
 }
--- demo/src/main/java/com/example/demo/ATM/CashWithdrawalManager.java
+++ demo/src/main/java/com/example/demo/ATM/CashWithdrawalManager.java
@@ -2,6 +2,9 @@ package com.example.demo.ATM;
 
 public abstract class CashWithdrawalManager {
     CashWithdrawalManager next;
+    public CashWithdrawalManager(){
+        next=null;
+    }
     public void setNext(CashWithdrawalManager next){
         this.next=next;
     }
